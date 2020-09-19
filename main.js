948c242eb55be0f1076d0041488a3f90fc602286
jide_omotola <jide@reach.africa>
2020-09-19 01:10:28 +0100

    delete function created

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 function calculate(val) {
   document.getElementById("display").value += val;
 }
+
 function solve() {
   let x = document.getElementById("display").value
   let y = eval(x)
   document.getElementById("display").value = y
 }
+
 function clr() {
   document.getElementById("display").value = ""
+}
+
+function del() {
+  var displayText = document.getElementById("display").value;
+  document.getElementById("display").value = displayText.substr(0, displayText.length - 1);
 }
